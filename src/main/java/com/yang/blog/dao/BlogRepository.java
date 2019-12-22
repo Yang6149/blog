d2c4ab4637250cb5e9d8539ac643405fd69104bd
Yang shen <614989768@qq.com>
2019-12-22 23:43:02 +0800

    部署在云成功  需把content类型改为text

--- src/main/java/com/yang/blog/dao/BlogRepository.java
+++ src/main/java/com/yang/blog/dao/BlogRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface BlogRepository extends JpaRepository<Blog,Long>, JpaSpecificationExecutor<Blog> {
-    @Query(value = "select * from t_Blog  order by id desc limit 0,?1",nativeQuery=true)
+    @Query(value = "select * from t_blog  order by id desc limit 0,?1",nativeQuery=true)
     List<Blog> findTop(int limit);
 
 
