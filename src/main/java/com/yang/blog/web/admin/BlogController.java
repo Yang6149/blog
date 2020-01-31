0a74ba92d93de1a5d584458a5ddab9abb75a1fbd
Yang shen <614989768@qq.com>
2020-01-31 19:44:04 +0800

    修改about页面的一些信息

--- src/main/java/com/yang/blog/web/admin/BlogController.java
+++ src/main/java/com/yang/blog/web/admin/BlogController.java
@@ -15,7 +15,6 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,7 +23,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import java.util.Date;
 
 @Controller
 @RequestMapping("/admin")
